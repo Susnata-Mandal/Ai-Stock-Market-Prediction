ea5483ea311d8766ef4effff1c96ea4a09b20c58
Susnata_Mandal <susnatamandal30@gmail.com>
2025-09-27 09:54:07 +0530

    Add files via upload

---
+++ 2_Contact_Us.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Contact Us')
+
+def contact_us():
+    st.markdown("""
+    Having any problem with our website?? 
+                
+    Feel free to reach out to us at susnatamandal30@gmail.com
+    """)
+
+if __name__ == "__main__":
+    contact_us()
